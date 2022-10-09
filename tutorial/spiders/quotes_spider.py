ca5c1468ac4cb575a33e60ed06e41de9b4914f6a
raymond <raymondan1031@gmail.com>
2022-10-09 20:44:18 +0800

    simpler start_requests

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -19,4 +19,17 @@ class QuotesSpider(scrapy.Spider):
             f.write(response.body)
         self.log(f'Save file {filename}')
 
+
+# class QuotesSpider(scrapy.Spider):
+#     name = "quotes"
+#     start_urls = [
+#         'https://quotes.toscrape.com/page/1/',
+#         'https://quotes.toscrape.com/page/2/',
+#     ]
+
+#     def parse(self, response):
+#         page = response.url.split("/")[-2]
+#         filename = f'quotes-{page}.html'
+#         with open(filename, 'wb') as f:
+#             f.write(response.body)
         
